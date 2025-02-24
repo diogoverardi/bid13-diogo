026a5be16992ca3a2205559ade01bfd7fee69475
Diogo Verardi <verardidiogo@gmail.com>
2025-02-24 02:40:48 -0500

    Added comments that explain my thought process and changes made on Question 1's code

--- Q1.php
+++ Q1.php
@@ -51,3 +51,28 @@ $result = isValidPhoneNumber($phone_number, $customer_id, $api_key);
 var_dump($result);
 
 #Diogo Verardi - 24FEB2025 - Quebec,Canada
+
+/**
+ * 
+
+Analysis and modifications 
+
+
+- This is the response the you'll get if you try and debug the code from chatgpt:
+string(218) "{"errors": [{"code": -40005, "description": "Method Not Allowed"}], "reference_id": null, "status": {"code": 500, "description": "Transaction not attempted", "updated_on": "2025-02-18T11:41:39Z"}, "sub_resource": null}" 
+
+and this is because the API Request is missing data, such as the actual Request body (CURLOPT_POSTFIELDS), after reviewing their documentation (https://developer.telesign.com/enterprise/reference/submitphonenumberforidentity) if you simply add this line it works:    curl_setopt($ch, CURLOPT_POSTFIELDS, '{"consent":{"method":1}}');
+
+    
+- I have also added validation for the parameter $phone_number.
+
+
+- I strongly suggest moving the variables $api_url and $valid_types to Constants. I didn't do it because this code example doesn't use classes, but in real-world I'd 100% have done it. 
+
+
+- Instead of repeating lines of code with curl_setopt() I have used curl_setopt_array(), readability is improved.
+
+- Added the JSON parameter in the CURL HTTPHEADER, as their API requests in the PHP example here: https://developer.telesign.com/enterprise/reference/submitphonenumberforidentity
+
+- The final error was array key "$data['numbering']['phone_type']" which is just completely wrong, a simple look in the API documentation or even a var_dump($data) will give you the right location for this data, which is $data['phone_type']['description']
+ /
